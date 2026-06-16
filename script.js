d1a48a6e1d70df3aa9efbfa4c89688a4ce5e3418
Muhammad Anas <daggaanas0888@gmail.com>
2026-06-16 11:17:40 +0530

    add innerHtml to show text

---
+++ script.js
@@ -0,0 +1,7 @@
+let mainArray = []
+mainArray.push("hello");
+mainArray.push("hehe");
+mainArray.push("haha");
+
+const mainArea = document.querySelector('.typing-space');
+mainArea.innerHTML = mainArray.join(" , ");
